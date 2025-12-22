3b89a89e571df930a6f623180899c5caaded98b0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-22 21:28:16 +0530

    commited by n8n

---
+++ backend/models/product.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from backend.config.config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f'<Product {self.name}>'
