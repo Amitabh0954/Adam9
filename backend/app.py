56b1b64c00e8b7cf99c69dc3db4e71dbca9772f5
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-23 13:02:19 +0530

    commited by n8n

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ from flask import Flask
 from backend.config.config import Config
 from backend.models.user import db
 from backend.models.product import Product
+from backend.models.category import Category
+from backend.models.product_category import product_category
 from backend.controllers.auth.user_controller import auth_bp
 from backend.controllers.auth.login_controller import login_bp
 from backend.controllers.auth.password_reset_controller import reset_bp
 from backend.controllers.auth.profile_controller import profile_bp
 from backend.controllers.products.product_controller import product_bp
+from backend.controllers.products.category_controller import category_bp
 
 def create_app():
     app = Flask(__name__)
@@ -19,6 +22,7 @@ def create_app():
     app.register_blueprint(reset_bp, url_prefix='/api/auth')
     app.register_blueprint(profile_bp, url_prefix='/api/auth')
     app.register_blueprint(product_bp, url_prefix='/api')
+    app.register_blueprint(category_bp, url_prefix='/api')
     
     return app
 
