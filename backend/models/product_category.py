7ff5f02cafca05f01a6c25e961b87829eac6f54d
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-23 13:02:16 +0530

    commited by n8n

---
+++ backend/models/product_category.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+product_category = db.Table('product_category',
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('categories.id'), primary_key=True)
+)
